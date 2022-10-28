4adea06808c71b67e54e06b9533e6a46669a6431
PedroMakengo <pedromakengomiguel@gmail.com>
2022-10-28 19:49:20 +0000

    Trabalhando com parte do meu formulário

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,7 +1,10 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div class="task">
-    <h1>Componente Task</h1>
+  <div id="task">
+    <form @submit.prevent="">
+      <input type="text" v-model="task" placeholder="Tarefa de hoje?" />
+      <button type="submit">Adicionar</button>
+    </form>
   </div>
 </template>
 
@@ -9,4 +12,42 @@
 export default {};
 </script>
 
-<style scoped></style>
+<style scoped>
+#task {
+  max-width: 700px;
+  background: #fff;
+  border-radius: 4px;
+  padding: 20px;
+
+  margin: 20px auto;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+}
+
+form {
+  margin-top: 30px;
+  display: flex;
+  flex-direction: row;
+}
+
+form button {
+  cursor: pointer;
+  background: #0f5959;
+  border: 0;
+  border-radius: 4px;
+  margin-left: 10px;
+  padding: 0 15px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: white;
+}
+
+input {
+  flex: 1;
+  border: 1px solid #eee;
+  padding: 6px 10px;
+  font-size: 14px;
+  outline: none;
+}
+</style>
