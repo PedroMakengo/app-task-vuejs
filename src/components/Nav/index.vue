bc86415502759b4e0ab9684d87795c03a2f57dba
PedroMakengo <pedromakengomiguel@gmail.com>
2022-10-29 18:11:49 +0000

    Adicionando todos os componentes do header

---
+++ src/components/Nav/index.vue
@@ -0,0 +1,13 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <nav id="nav"></nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+#nav {
+}
+</style>
