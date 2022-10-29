91755961dd8546b42947fcc3fc4ede6c85d32261
PedroMakengo <pedromakengomiguel@gmail.com>
2022-10-29 18:06:40 +0000

    Organizando os meus components

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# app-task-vuejs
-Desenvolvendo uma app para adicionar tarefas com o vuejs e typescript, este é um projeto do curso de vuejs com o desafio de desenvolver à parte prática e melhorar em alguns aspectos.
+# coral-ecommerce-vuejs
+Desafio prático, coral é um template encontrado no figma community sendo assim UI/UX de um e-commerce muito lindo, desta feita achei interessante colocar em práticas algumas coisas que tenho adquirido neste design.
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,13 @@
 <template>
   <div class="app">
-    <header>Minhas Tarefas</header>
-
-    <Task />
+    <h1>Coral</h1>
   </div>
 </template>
 
 <script>
-import Task from "@/components/Task.vue";
-
 export default {
   name: "App",
-  components: {
-    Task,
-  },
 };
 </script>
 
-<style>
-header {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 80px;
-  font-size: 40px;
-  color: #fff;
-}
-</style>
+<style></style>
