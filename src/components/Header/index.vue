232b9cd0abc577fdafd8e904c141ed3ee5b1c6c7
PedroMakengo <pedromakengomiguel@gmail.com>
2022-10-29 18:09:58 +0000

    Adicinando o meu header

---
+++ src/components/Header/index.vue
@@ -0,0 +1,13 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <header id="header"></header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+#header {
+}
+</style>
