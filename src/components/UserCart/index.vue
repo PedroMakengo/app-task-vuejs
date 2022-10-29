48e7d2e563d38ba110bb8352327eb582fa28a6e6
PedroMakengo <pedromakengomiguel@gmail.com>
2022-10-29 19:05:02 +0000

    Adicionando

---
+++ src/components/UserCart/index.vue
@@ -0,0 +1,22 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div id="user-cart">
+    <a href="#">
+      <img src="" alt="" />
+      Account
+    </a>
+    <a href="#">
+      <img src="" alt="" />
+      Shopping
+    </a>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+#user-cart {
+}
+</style>
