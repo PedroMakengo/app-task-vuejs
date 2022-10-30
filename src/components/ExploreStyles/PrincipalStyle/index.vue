2087dad1518c10fa2ad5d7cbf28cd69a5d3a2b45
PedroMakengo <pedromakengomiguel@gmail.com>
2022-10-30 07:20:14 +0000

    Trabalhando

--- src/components/ExploreStyles/PrincipalStyle/index.vue
+++ src/components/ExploreStyles/PrincipalStyle/index.vue
@@ -1,11 +1,21 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div class="bg-style style__principal">
-    <div class="ticket">
-      <span>Name</span>
-      <span>103 Product</span>
-    </div>
-  </div>
+  <div class="style__principal"></div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.style__principal::before {
+  content: "Explorer New and Popular Styles";
+  transform: rotate(-90deg);
+
+  text-transform: uppercase;
+
+  display: block;
+  font-weight: 300;
+  font-size: 1.45rem;
+
+  position: relative;
+  top: 36%;
+  left: -55%;
+}
+</style>
