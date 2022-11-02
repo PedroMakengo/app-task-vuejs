0d41ed4cb4a385257d186100d5d79ce3fef0849c
PedroMakengo <pedromakengomiguel@gmail.com>
2022-11-02 17:28:37 +0000

    Adicionando a estrutura do projeto

---
+++ src/components/FollowProduct/index.vue
@@ -0,0 +1,12 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <section id="brand">
+    <div class="container">
+      <ul>
+        <li v-for="(item, index) in brand" :key="index">
+          <img :src="item.image" alt="" />
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
