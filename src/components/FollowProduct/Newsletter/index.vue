e56da3a97a00ae6b6d1bb2323232508c4ba86a97
PedroMakengo <pedromakengomiguel@gmail.com>
2022-11-03 07:59:55 +0000

    Trabalhando

--- src/components/FollowProduct/Newsletter/index.vue
+++ src/components/FollowProduct/Newsletter/index.vue
@@ -11,3 +11,46 @@
     </div>
   </div>
 </template>
+
+<style scoped>
+#newsletter {
+  text-align: center;
+}
+
+#newsletter .container h3 {
+  font-family: "Roboto", sans-serif;
+  font-size: 2rem;
+  font-weight: 500;
+  text-transform: capitalize;
+  margin-bottom: 2rem;
+}
+
+form {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 1rem;
+}
+
+form input {
+  width: 50%;
+  border: 0;
+  background: none;
+  outline: none;
+}
+
+form input,
+form button {
+  padding: 1rem;
+  border-bottom: 1px solid var(--text-color-link);
+}
+
+form input:focus {
+  border-bottom: 1px solid var(--bg-btn) !important;
+}
+
+form button {
+  text-transform: uppercase;
+  font-weight: bold;
+}
+</style>
---
+++ src/components/Footer/index.vue
@@ -0,0 +1,6 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <section id="footer">
+    <div class="container"></div>
+  </section>
+</template>
